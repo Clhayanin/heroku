df6aedc03c547389f1e1198bf54b81a6bb7e48af
chayanin <6106021612190@fitm.kmutnb.ac.th>
2021-04-24 21:29:09 +0700

    Add flask-Heroku project to GitHub remote

---
+++ front/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class=" h-screen">
+    <title_ITB class=""/>
+    <title_news />
+    <title_post />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import title_post from '../components/title_post.vue'
+import title_news from '../components/title_news.vue'
+import title_ITB from '../components/title.vue' 
+export default {
+  name: 'Home',
+  loadtest:"",
+  components: {
+    title_ITB,
+    title_news,
+    title_post
+  },
+
+
+}
+</script>
